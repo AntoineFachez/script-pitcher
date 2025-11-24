f0d77027854c1195876e716ede24a65950e7c343
Anthony Zornig <85788734+AntoineFachez@users.noreply.github.com>
2025-11-24 08:42:37 +0100

    refactor Drawer

--- src/components/drawer/Drawer.js
+++ src/components/drawer/Drawer.js
@@ -3,7 +3,7 @@ import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Divider from "@mui/material/Divider";
 import MenuIcon from "@mui/icons-material/Menu";
-import { IconButton } from "@mui/material";
+import { IconButton, Toolbar } from "@mui/material";
 import { ArrowBack, Close, ViewSidebar } from "@mui/icons-material";
 
 export default function BasicDrawer({
@@ -16,15 +16,19 @@ export default function BasicDrawer({
   anchor,
   iconToOpen,
 }) {
-  // console.log("handleToggleDrawer", orientationDrawer.bottom);
   const list = (anchor) => {
     // setOrientationDrawer({ ...orientationDrawer, [anchor]: true })
     return (
       <Box
-        sx={{
-          width: anchor === "top" || anchor === "bottom" ? "auto" : "100%",
-          overflowY: "auto",
-        }}
+        sx={
+          {
+            // width: anchor === "top" || anchor === "bottom" ? "auto" : "100%",
+            // overflowY: "auto",
+            // display: "flex",
+            // justifyContent: "center",
+            // alignItems: "center",
+          }
+        }
         role="presentation"
       >
         <Divider />
@@ -47,36 +51,40 @@ export default function BasicDrawer({
         anchor={anchor}
         open={orientationDrawer[anchor]}
         onClose={handleToggleDrawer(anchor, false)}
-        sx={{
-          "& .MuiDrawer-paper": {
-            width: anchor === "left" ? "fit-content" : null,
-            // marginTop: "3rem",
-            // height: "80vh",
-            // bottom: 0,
-            display: "flex",
-            justifyContent: "flex-start",
-            overflowY: "hidden",
-          },
-        }}
         slotProps={{
           paper: {
             sx: {
+              width:
+                anchor === "left"
+                  ? "fit-content"
+                  : anchor === "right"
+                  ? "40%"
+                  : 500,
               zIndex: 1500, // Choose a value higher than the Modal (default 1300)
             },
           },
         }}
       >
-        <Box>
-          <IconButton onClick={() => goBack("")}>
+        <Toolbar
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "flex-end",
+            p: 0,
+            m: 0,
+            // backgroundColor: "white",
+          }}
+        >
+          {/* <IconButton onClick={() => goBack("")}>
             <ArrowBack />
-          </IconButton>
+          </IconButton> */}
           <IconButton
             onClick={handleToggleDrawer(anchor, !orientationDrawer[anchor])}
           >
             <Close />
           </IconButton>
           {menu}
-        </Box>
+        </Toolbar>
         {list(anchor)}
       </Drawer>
     </>
